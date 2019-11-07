f0362cfc610dd925ff0b6e7429c1a06cab0db6a7
Ahmed Jamal Tariq <40691417+ahmedjamaltariq@users.noreply.github.com>
2019-11-07 23:38:08 +0500

    Update Downloader.php

--- downloader.php
+++ downloader.php
@@ -1,7 +1,7 @@
 <?php
         $options  = array('http' => array('user_agent' => 'custom user agent string'));
         $context  = stream_context_create($options);
-        $response = file_get_contents($request->get('link'), false, $context);
+        $response = file_get_contents('__PASTE_FACEBBOOK_VIDEO_LINK_HERE__', false, $context);
         preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', strip_tags($response), $match);
         $searchword = 'video';
         $matches = array_filter($match[0], function($var) use ($searchword) { return preg_match("/\b$searchword\b/i", $var); });
